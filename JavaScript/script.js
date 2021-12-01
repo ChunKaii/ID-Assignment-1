da47a592b4d45d851e9e89d7f45739c65a5ac2c9
ChunKaii <s10222222@connect.np.edu.sg>
2021-12-01 22:23:15 +0800

    Added thank you page upon submitting email

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,3 +1,22 @@
-function confirmSubmission(){
-    alert("Are you ready to submit?");
+function thankyouMessage(){
+    let x = document.getElementById("message");
+    if (x.style.display == "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function reset(){
+    let x = document.getElementById("message");
+    x.style.display = "none";
+}
+
+function displayImage(src, width, height) {
+    let img = document.createElement("img");
+
+    img.src = src;
+    img.width = width;
+    img.height = height;
+
+    document.body.appendChild(img);
 }
