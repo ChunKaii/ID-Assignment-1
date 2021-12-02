e1278d974640baafba1811686f9dbc78a70ecc7e
ChunKaii <s10222222@connect.np.edu.sg>
2021-12-02 09:45:09 +0800

    Addition of comments

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,20 +1,8 @@
+//Function to check with user if they want to submit the email
+//and should they confirm, it will send them to the thank you page
 function alertuser(){
     let answer = confirm("Are you ready to submit with all your desired information inputted?");
     if (answer){
         window.location.href='thank you page.html';
     }
-}
-function reset(){
-    let x = document.getElementById("message");
-    x.style.display = "none";
-}
-
-function displayImage(src, width, height) {
-    let img = document.createElement("img");
-
-    img.src = src;
-    img.width = width;
-    img.height = height;
-
-    document.body.appendChild(img);
 }
